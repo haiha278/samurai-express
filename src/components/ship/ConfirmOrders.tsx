970e0e5445b85544f7df16e9663aad852c7e0144
LE HAI HA <haiha278@lgcns.com>
2024-10-30 11:06:39 +0700

    update UI

--- src/components/ship/ConfirmOrders.tsx
+++ src/components/ship/ConfirmOrders.tsx
@@ -1,9 +1,10 @@
 import Image from "next/image";
 import { useCart } from "@/hook/CartContext";
 import { useState } from "react";
+import QRCode from "@/assets/image/qrCode.jpg";
 export const ConfirmOrders = () => {
   const { cart, totalPriceOfAllItem } = useCart();
-  const [selectedOption, setSelectedOption] = useState<string>();
+  const [selectedOption, setSelectedOption] = useState<string>("card");
 
   const handleSelectPaidOption = (id: string) => {
     setSelectedOption(id);
@@ -18,7 +19,7 @@ export const ConfirmOrders = () => {
           {cart.items.map((item) => (
             <div
               key={item.id}
-              className="flex items-center justify-between p-2"
+              className="flex items-center justify-between p-4"
             >
               <div className="flex items-center gap-4">
                 <div>
@@ -42,26 +43,26 @@ export const ConfirmOrders = () => {
               </div>
             </div>
           ))}
-          <div className="flex px-2 justify-between items-center mt-8">
-            <div className="text-base text-gray-800 bg-gray-100 p-2 w-4/5">
+          <div className="flex px-4 justify-between items-center mt-8">
+            <div className="text-base text-gray-800 bg-gray-100 p-4 w-4/5">
               Tạm Tính
             </div>
             <div className="text-red-600 font-semibold">
               {totalPriceOfAllItem()}
             </div>
           </div>
-          <div className="flex p-2 justify-between items-center">
-            <div className="text-base text-gray-800 bg-gray-100 p-2 w-4/5">
+          <div className="flex p-4 justify-between items-center">
+            <div className="text-base text-gray-800 bg-gray-100 p-4 w-4/5">
               Tổng
             </div>
             <div className="text-red-600 font-semibold">
               {totalPriceOfAllItem()}
             </div>
           </div>
-          <div className="px-2 mt-8 text-base">
+          <div className="px-4 mt-8 text-base">
             (Chưa bao gồm phí vận chuyển)
           </div>
-          <div className="text-red-600 mt-4 px-2">
+          <div className="text-red-600 mt-4 px-4">
             * VUI LÒNG CHỌN HÌNH THỨC THANH TOÁN
           </div>
           <div
@@ -83,6 +84,28 @@ export const ConfirmOrders = () => {
             </div>
             <span className="font-semibold">Chuyển khoản ngân hàng</span>
           </div>
+          {selectedOption === "card" && (
+            <div>
+              <div>
+                <Image
+                  width={300}
+                  height={300}
+                  src={QRCode}
+                  alt="qr_image"
+                  className="mx-auto"
+                />
+              </div>
+              <div className="flex flex-col gap-[15px] mt-[20px] p-4">
+                <div>
+                  Chủ TK: <strong>Vũ Lê Minh</strong>
+                </div>
+                <div>
+                  Số TK: <strong>0866177929</strong>
+                </div>
+                <div>Nội dung : Tên khách hàng + SĐT đặt hàng</div>
+              </div>
+            </div>
+          )}
           <div
             className="flex items-center gap-4 bg-gray-50 p-4 mx-4 my-4 cursor-pointer hover:bg-gray-100"
             onClick={() => handleSelectPaidOption("cod")}
@@ -101,6 +124,23 @@ export const ConfirmOrders = () => {
               ></div>
             </div>
             <span className="font-semibold">Trả tiền mặt khi nhận hàng</span>
+          </div>
+          {selectedOption === "cod" && (
+            <div>
+              <div className="text-gray-500 text-[15px] px-[40px]">
+                Trả tiền mặt khi giao hàng
+              </div>
+            </div>
+          )}
+          <div className="text-gray-500 px-4 mt-[40px]">
+            Bằng việc bấm nút đặt hàng, tôi xác nhận đã đọc và đồng ý với{" "}
+            <a href="#" className="text-topicColor underline">
+              điều khoản và điều kiện
+            </a>{" "}
+            của website.
+          </div>
+          <div className="mx-4 py-4 bg-topicColor text-white text-center rounded-3xl mt-[50px] mb-[20px]">
+            Đặt Hàng
           </div>
         </>
       ) : (
